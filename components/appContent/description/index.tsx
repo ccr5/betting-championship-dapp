f13b025d17cb104297d2ea2a222b99ecb5007e2b
ccr5 <matt-gomes@live.com>
2022-06-26 09:58:11 -0300

    App screen layout Ok

--- components/appContent/description/index.tsx
+++ components/appContent/description/index.tsx
@@ -1,10 +1,10 @@
 export default function Description() {
   return (
-    <div className="w-full h-full p-3 text-white m-3">
+    <div className="w-full h-full p-2 text-white m-2">
       <div className="h-1/3">
-        <h4 className="font-bold">Objective</h4>
+        <h4 className="font-bold text-3xl">Objective</h4>
       </div>
-      <div className="h-2/3">
+      <div className="text-lg h-2/3">
         Lorem ipsum dolor sit amet, consectetur
         adipiscing elit, sed do eiusmod tempor incididunt
         ut labore et dolore magna aliqua.
