c1049d8426d452a99776b95ede77ede6266ce21f
ccr5 <matt-gomes@live.com>
2022-07-07 23:15:19 -0300

    Cleaning unfinished parts of the project

--- components/appContent/game/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Scene } from 'three';
-
-export default function Game() {
-  const scene = new Scene();
-
-  return (
-    <canvas className="bg-paletteFive min-h-[500px] h-full w-full rounded-3xl" />
-  )
-}
